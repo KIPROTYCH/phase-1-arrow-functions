b5dab4649d9aa4b56b86aeb776025ec2ce280884
KIPROTYCH <kiprotych@gmail.com>
2023-06-14 22:33:14 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+
+const divide = () => {
+    return 2000 / 100;
+};
+
+const square = (num) => {
+    return num * num;
+};
+
+const add = (num1, num2) => {
+    return num1 + num2;
+};
